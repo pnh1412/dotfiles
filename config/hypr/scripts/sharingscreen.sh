0d2ff01717aad77856344556156179bc436a8d05
pnh1412 <82204690+pnh1412@users.noreply.github.com>
2023-01-26 03:15:59 +0700

    Add files via upload

---
+++ config/hypr/scripts/sharingscreen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 1
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal
+/usr/libexec/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
+
+#httpd
+xhost +local:
+xhost +si:localuser:root
